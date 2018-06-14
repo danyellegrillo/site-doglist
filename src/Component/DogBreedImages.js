09a77eb09d16a66f9201d30dbb628d33cfbb2547
DANYELLEGRILLO <dany_varre@hotmail.com>
2018-06-14 14:10:27 +0200

    project ready

--- src/Component/DogBreedImages.js
+++ src/Component/DogBreedImages.js
@@ -7,6 +7,7 @@ export default function DogBreedImages (props) {
     <div className="dog-breed-images">
       <h1>Dogs Breed Images</h1>
 
+
       <Link to="/">Go back to the index</Link>
       <div>
         { props.images && props.images.map(url => <img src={ url } alt="Dog" />) }
